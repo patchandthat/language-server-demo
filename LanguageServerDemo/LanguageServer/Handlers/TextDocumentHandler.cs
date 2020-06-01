ecf4d78ecfe2970a459c5ac9c19b91688993b6a4
Patrick Allwood <patchandthat@gmail.com>
2020-06-01 22:06:29 +0100

    Demo language server

---
+++ LanguageServerDemo/LanguageServer/Handlers/TextDocumentHandler.cs
@@ -0,0 +1,71 @@
+﻿using MediatR;
+using OmniSharp.Extensions.LanguageServer.Protocol;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Server;
+using OmniSharp.Extensions.LanguageServer.Protocol.Server.Capabilities;
+using OmniSharp.Extensions.LanguageServer.Protocol.Window;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoLanguageServer.Handlers
+{
+    public class TextDocumentHandler : TextDocumentSyncHandler
+    {
+        private readonly ILanguageServer _server;
+
+        public TextDocumentHandler(ILanguageServer server) : 
+            base(TextDocumentSyncKind.Full, new TextDocumentSaveRegistrationOptions()
+            {
+                DocumentSelector = new DocumentSelector(
+                    new DocumentFilter()
+                    {
+                        Pattern = "**/*.demo"
+                    }),
+                IncludeText = true
+            })
+        {
+            _server = server;
+
+            _server.Window.LogMessage(new LogMessageParams
+            {
+                Message = "Handler created",
+                Type = MessageType.Info
+            });
+        }
+
+        public override TextDocumentAttributes GetTextDocumentAttributes(DocumentUri uri)
+        {
+            return new TextDocumentAttributes(uri, "demo");
+        }
+
+        public override Task<Unit> Handle(DidOpenTextDocumentParams request, CancellationToken cancellationToken)
+        {
+            var message = "Opened demo file!";
+
+            _server.Window.ShowMessage(new ShowMessageParams
+            {
+                Message = message,
+                Type = MessageType.Info
+            });
+
+            return Task.FromResult(Unit.Value);
+        }
+
+        public override Task<Unit> Handle(DidChangeTextDocumentParams request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Unit.Value);
+        }
+
+        public override Task<Unit> Handle(DidSaveTextDocumentParams request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Unit.Value);
+        }
+
+        public override Task<Unit> Handle(DidCloseTextDocumentParams request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Unit.Value);
+        }
+    }
+}
---
+++ LanguageServerDemo/LanguageServer/Program.cs
@@ -0,0 +1,43 @@
+﻿using DemoLanguageServer.Handlers;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Window;
+using OmniSharp.Extensions.LanguageServer.Server;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoLanguageServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var server = await LanguageServer.From(options =>
+                options
+                    .WithInput(Console.OpenStandardInput())
+                    .WithOutput(Console.OpenStandardOutput())
+                    .WithHandler<TextDocumentHandler>()
+                    .OnStarted(OnStartedCallback)
+                );
+
+
+            await server.WaitForExit;
+        }
+
+        private static Task OnStartedCallback(ILanguageServer server, InitializeResult result, CancellationToken cancellationToken)
+        {
+            server.Window.ShowMessage(new ShowMessageParams
+            {
+                Message = $"{result.ServerInfo.Name} server started",
+                Type = MessageType.Info
+            });
+
+            return Task.CompletedTask;
+        }
+    }
+}
