81540f6db67f21d1c980057f735cfc0fa87e2907
Patrick Allwood <patchandthat@gmail.com>
2020-06-01 23:13:43 +0100

    VS Extension

---
+++ LanguageServerDemo/DemoLangVSExtension/DemoLangContentDefinition.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace DemoLangVSExtension
+{
+    public class DemoLangContentDefinition
+    {
+        [Export]
+        [Name("demo")]
+        [BaseDefinition(CodeRemoteContentDefinition.CodeRemoteContentTypeName)]
+        internal static ContentTypeDefinition BarContentTypeDefinition;
+
+        [Export]
+        [FileExtension(".demo")]
+        [ContentType("demo")]
+        internal static FileExtensionToContentTypeDefinition BarFileExtensionDefinition;
+    }
+}
